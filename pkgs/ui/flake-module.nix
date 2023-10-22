e5c0bc7fd45b2e8a4cff22659e4a1fae1b18e31f
Qubasa <consulting@qube.email>
2023-10-22 22:38:04 +0200

    Fixed AnyURL problem

--- pkgs/ui/flake-module.nix
+++ pkgs/ui/flake-module.nix
@@ -8,6 +8,7 @@
     {
       packages = {
         ui = base.pkg.global;
+        theme = base.pkg.theme;
         ui-assets = pkgs.callPackage ./nix/ui-assets.nix { };
         # EXAMPLE: GITEA_TOKEN=$(rbw get -f GITEA_TOKEN git.clan.lol) nix run .#update-ui-assets
         update-ui-assets = pkgs.callPackage ./nix/update-ui-assets.nix { };
