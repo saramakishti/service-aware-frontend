7a354875c9d64289e1bc81a9f63fc8b1b2e03396
Qubasa <consulting@qube.email>
2023-10-23 01:23:06 +0200

    Fully working ui and cli

--- pkgs/clan-cli/clan_cli/webui/api_outputs.py
+++ pkgs/clan-cli/clan_cli/webui/api_outputs.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, Field
 
 from ..async_cmd import CmdOut
 from ..task_manager import TaskStatus
-from ..vms.inspect import VmConfig
 
 
 class Status(Enum):
@@ -19,52 +18,4 @@ class Machine(BaseModel):
     status: Status
 
 
-class MachineCreate(BaseModel):
-    name: str
 
-
-class MachinesResponse(BaseModel):
-    machines: list[Machine]
-
-
-class MachineResponse(BaseModel):
-    machine: Machine
-
-
-class ConfigResponse(BaseModel):
-    config: dict
-
-
-class SchemaResponse(BaseModel):
-    schema_: dict = Field(alias="schema")
-
-
-class VmStatusResponse(BaseModel):
-    error: str | None
-    status: TaskStatus
-
-
-class VmCreateResponse(BaseModel):
-    uuid: str
-
-
-class FlakeAttrResponse(BaseModel):
-    flake_attrs: list[str]
-
-
-class VmInspectResponse(BaseModel):
-    config: VmConfig
-
-
-class FlakeAction(BaseModel):
-    id: str
-    uri: str
-
-
-class FlakeCreateResponse(BaseModel):
-    cmd_out: Dict[str, CmdOut]
-
-
-class FlakeResponse(BaseModel):
-    content: str
-    actions: List[FlakeAction]
