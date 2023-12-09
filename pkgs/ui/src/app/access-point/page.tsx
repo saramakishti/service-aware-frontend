5262d83da08d5b0468be0bb85fa6e623bd1d5595
Onur Arslan <runoxa@hotmail.com>
2023-12-10 00:15:34 +0100

    [Functionality] Request DID Resolution - fetch resolution data - adjustments

--- pkgs/ui/src/app/access-point/page.tsx
+++ pkgs/ui/src/app/access-point/page.tsx
@@ -28,17 +28,7 @@ export default function AccessPoint() {
       .then((resp) =>
         resp.json().then((jsonData) => {
           console.log(jsonData);
-
-          const transformedData = jsonData.map(
-            (item: { service_name: any; entity_did: any; network: any }) => ({
-              entity_name: item.service_name,
-              entity_did: item.entity_did,
-              network: item.network,
-              ip_address: "",
-            }),
-          );
-
-          setRepositoryData(transformedData);
+          setRepositoryData(jsonData);
         }),
       )
       .then()
