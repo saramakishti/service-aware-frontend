1e7bbb0aba95a32d3d6c0612515a60feac680bb9
Arslan, Erdem <erdem.arslan@valtech.com>
2024-01-13 17:28:17 +0100

    fix order of tailwind classes 2

--- pkgs/ui/src/components/sidebar/index.tsx
+++ pkgs/ui/src/components/sidebar/index.tsx
@@ -185,7 +185,7 @@ export function Sidebar(props: SidebarProps) {
                         primaryTypographyProps={{
                           color: "inherit",
                         }}
-                        className="lg:block hidden mr-4"
+                        className="lg:block mr-4 hidden"
                       />
                       {collapseMenuOpen ? <ExpandLess /> : <ExpandMore />}
                     </ListItemButton>
